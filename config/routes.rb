a9914e7f5733ca4434eb69263be414a44e04365d
kchohee <kchohee0630@gmail.com>
2020-05-08 10:47:54 -0500

    created welcome controller and corrected root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :characters
-  root to: 'application#home'
+  root to: 'welcome#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
