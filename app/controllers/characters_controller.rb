87e2fdb7cc5d0f78dee632ce2f65a373756afafc
kchohee <kchohee0630@gmail.com>
2020-05-10 19:54:53 -0500

    made characters nested under users.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,50 @@
 class CharactersController < ApplicationController
+    def index
+        @characters = Character.all
+      end
+      
+      def show
+        @character = Characters.find_by(id: params[:id])
+      end
+      
+      def new
+        @character = Character.new
+      end
+      
+      def create
+        @character = Character.create(character_params)
+        if @character.save
+          redirect_to user_character_path(@character)
+        else
+          render ':new'
+        end
+      end
+      
+      def edit
+        @character = Character.find(params[:id])
+      end
+    
+      def update
+        @character = Character.find(params[:id])
+        @character.update(character_params)
+        if @character.save
+          redirect_to @character
+        else
+          render :edit
+        end
+      end
+    
+      def destroy
+        @character = Character.find(params[:id])
+        @character.destroy
+        flash[:notice] = "Character deleted."
+        redirect_to characters_path
+      end
+
+      private
+
+      def character_params
+        params.require(:character).permit(:user_id, :name, :race, :level, :background)
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "omniauth"}
   root to: 'welcome#home'
-  resources :characters
-  resources :users
+  resources :users do
+    resources :characters
+  end
+  
   get '/user' => "users#show", :as => :user_root
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
