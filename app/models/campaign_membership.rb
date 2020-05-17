fdf0162e578a66269d42f3fc23fa215fbe2a312d
kchohee <kchohee0630@gmail.com>
2020-05-17 11:30:57 -0500

    removed files from cm to membership and new index

--- app/models/campaign_membership.rb
+++
@@ -1,5 +0,0 @@
-class CampaignMembership < ApplicationRecord
-    belongs_to :user
-    belongs_to :campaign
-    belongs_to :character
-end
--- db/migrate/20200514191544_create_campaign_memberships.rb
+++
@@ -1,9 +0,0 @@
-class CreateCampaignMemberships < ActiveRecord::Migration[5.2]
-  def change
-    create_table :campaign_memberships do |t|
-      t.belongs_to :campaign, foreign_key: true
-      t.belongs_to :user, foreign_key: true
-      t.belongs_to :character, foreign_key: true
-    end
-  end
-end
