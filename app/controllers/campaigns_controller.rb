41abf208b93bc6ac4a6b552d26f441c814dc8ee4
kchohee <kchohee0630@gmail.com>
2020-05-16 20:13:00 -0500

    corrected errors on users show

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -10,8 +10,11 @@ class CampaignsController < ApplicationController
         @campaign = Campaign.new
       end
       def create
-        @campaign = Campaign.create(campaign_params)
-        redirect_to user_campaign_path(@campaign)
+        @campaign = Campaign.new(campaign_params)
+        @campaign.owner_id = current_user.id
+        @campaign.users << current_user
+        @campaign.save
+        redirect_to campaign_path(current_user,@campaign)
       end
       def edit
         @campaign = Campaign.find_by(id: params[:id])
@@ -19,11 +22,15 @@ class CampaignsController < ApplicationController
       def update
         @campaign = Campaign.find_by(id: params[:id])
         @campaign.update(campaign_params)
-        redirect_to user_campaign_path(@campaign)
+        redirect_to campaign_path(current_user,@campaign)
+      end
+
+      def destroy
+        @campaign = Campaign.find(params[:id])
       end
 
     private
     def campaign_params
-        params.require(:user).permit(:name, :location, :story, :lvl, :owner_id)
+        params.require(:campaign).permit(:name, :location, :story, :lvl, :owner_id)
     end
 end
